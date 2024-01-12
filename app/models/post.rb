bef33757c64b054f84309c75c469abfd2f9eb663
Abdullah <geekyhacks22@gmail.com>
2024-01-12 23:51:43 +0300

    Create Post method that returns the 5 most recent comments for a given post and method that update the posts_conter for each user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,15 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
+
+  after_create :update_user_posts_counter
+
+  def most_recent_comments
+    comments.order(created_at: :desc).order(5)
+  end
+
+  private
+
+  def update_user_posts_counter
+    User.increment_counter(:posts_counter, author_id)
+  end
 end
