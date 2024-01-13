5e22d134180756c65faab1f3c4084bdc5ac7df9b
Abdullah <geekyhacks22@gmail.com>
2024-01-13 18:08:37 +0300

    Add post spec testing file

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,8 +13,9 @@ class Post < ApplicationRecord
     User.increment_counter(:posts_counter, author_id)
   end
 
-  # validations
+  #validations
   validates :title, presence: true, length: { maximum: 250 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,3 +1,41 @@
-RSpec.describe Post, type: :model do
+require 'rails_helper'
 
+RSpec.describe Post, type: :model do
+  subject { described_class.new(title: 'title', text: 'text') }
+
+  context 'testing the post data ' do
+    it 'validates that title should not be blank' do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+    it 'validates that text should not greater than 250' do
+      subject.text.length > 250
+      expect(subject).to_not be_valid
+    end
+    it 'validates that text should not be blank' do
+      subject.text = nil
+      expect(subject).to_not be_valid
+    end
   end
+
+  context 'testing the comments_counter' do
+    it 'validates that comments_counter is not blank' do
+      subject.comments_counter = nil
+      expect(subject).to_not be_valid
+    end
+    it 'validates that comments_counter is greater or equal to zero' do
+      subject.comments_counter = -1
+      expect(subject).to_not be_valid
+    end
+  end
+  context 'testing the likes_counter' do
+    it 'validates that likes_counter is not blank' do
+      subject.likes_counter = nil
+      expect(subject).to_not be_valid
+    end
+    it 'validates that likes_counter is greater or equal to zero' do
+      subject.likes_counter = -1
+      expect(subject).to_not be_valid
+    end
+  end
+end
