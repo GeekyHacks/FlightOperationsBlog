4f4fdb99008439f4357be366b25e375cb0c441cd
Abdullah <geekyhacks22@gmail.com>
2024-01-17 20:04:07 +0300

    Add posts testing

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,15 +3,34 @@ require 'rails_helper'
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      get '/posts/index'
+      get '/users/:user_id/posts'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'renders a correct index template' do
+      get '/users/:user_id/posts'
+      expect(response).to render_template(:index)
+    end
+
+    it 'include the correct placeholders' do
+      get '/users/:user_id/posts'
+      expect(response.body).to include('Find me in app/views/posts/index.html.erb')
     end
   end
 
   describe 'GET /show' do
     it 'returns http success' do
-      get '/posts/show'
+      get '/users/:user_id/posts/:id'
       expect(response).to have_http_status(:success)
+    end
+    it 'renders a correct index template' do
+      get '/users/:user_id/posts/:id'
+      expect(response).to render_template(:show)
+    end
+
+    it 'include the correct placeholders' do
+      get '/users/:user_id/posts/:id'
+      expect(response.body).to include('Find me in app/views/posts/show.html.erb')
     end
   end
 end
