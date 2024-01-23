182e03e674ff31cd6d7fd0a7151afcd8eb5fdfd5
Abdullah <geekyhacks22@gmail.com>
2024-01-23 22:31:45 +0300

    Add the posts index & show view files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = Post.where(author_id: params[:user_id]).order(author_id: :desc)
+    # @posts = @posts.paginate(page: params[:page], per_page: 1)
+  end
 
-  def show; end
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
 end
