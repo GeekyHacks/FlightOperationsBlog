a1230884903eee6b5da5df3386452e4feab42088
Abdullah <geekyhacks22@gmail.com>
2024-01-24 22:50:15 +0300

    Finish the posts views navigation part

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,11 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @posts = Post.where(author_id: params[:user_id]).order(author_id: :desc)
     # @posts = @posts.paginate(page: params[:page], per_page: 1)
+    @comments = Comment.all.order(id: :desc)
   end
 
   def show
     @post = Post.find_by(id: params[:id])
+    @comments = @post.comments.limit(5)
   end
 end
