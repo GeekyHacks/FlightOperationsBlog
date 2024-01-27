c2bae7bcec3972a287c8353cc468da4666b5ef15
Abdullah <geekyhacks22@gmail.com>
2024-01-27 22:01:44 +0300

    Make the post form functional

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,20 +8,52 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find_by(id: params[:id])
-    @comments = @post.comments.limit(5)
+    @comments = @post.most_recent_comments
   end
 
   def new
+    @user = User.find(params[:user_id])
     @post = Post.new
   end
 
+  # Create action to handle post creation
   def create
-    @post = current_user.posts.create(post_params)
+    @post = Post.new(post_params)
+    @post.author_id = current_user.id
+    @post.likes_counter = 0
+    @post.comments_counter = 0
     if @post.save
-      redirect_to user_posts_path(current_user)
+      @user = User.find(params[:user_id])
+      redirect_to user_post_path(@user, @post), notice: 'Post was successfully created.'
     else
-      render :new
+
+      pp @post.errors
+      pp @post.errors.full_messages
+      @user = User.find(params[:user_id])
+      render 'new'
     end
+  end
+
+  # Edit action to display the post edit form
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  # Update action to handle post updates
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Post was successfully updated.'
+    else
+      render 'edit'
+    end
+  end
+
+  # Destroy action to delete a post
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: 'Post was successfully deleted.'
   end
 
   private
