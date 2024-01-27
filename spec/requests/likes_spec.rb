040bea4c98ae80fcef8ff59f9200c4853e96791e
Abdullah <geekyhacks22@gmail.com>
2024-01-27 22:42:06 +0300

    Fix linter errors

--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -1,25 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe "Likes", type: :request do
-  describe "GET /new" do
-    it "returns http success" do
-      get "/likes/new"
+RSpec.describe 'Likes', type: :request do
+  describe 'GET /new' do
+    it 'returns http success' do
+      get '/likes/new'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/likes/create"
+  describe 'GET /create' do
+    it 'returns http success' do
+      get '/likes/create'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/likes/destroy"
+  describe 'GET /destroy' do
+    it 'returns http success' do
+      get '/likes/destroy'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
