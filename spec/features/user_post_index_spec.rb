37ae4df31f6d0cdab547a43fc9c10c2470b0eadb
Abdullah <geekyhacks22@gmail.com>
2024-02-02 17:50:05 +0300

    Edit user_show test case

--- spec/features/user_post_index_spec.rb
+++ spec/features/user_post_index_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
-RSpec.feature "UserPostIndices", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.describe 'User Post Index Page', type: :feature do
+  before do
+    @user = User.create(name: 'Abdu', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Yemen.',
+                        posts_counter: 0)
+    11.times { |i| Post.create(author: @user, title: "Post #{i}", text: "Content of post #{i}") }
+    visit user_posts_path(user)
+  end
+
+  it 'shows the user and posts details' do
+    expect(page).to have_selector("img[src*='#{@user.photo}']")
+    expect(page).to have_content(@user.name)
+    expect(page).to have_content("Number of posts: #{@user.posts_counter}")
+
+    visible_posts = user.posts.includes(:comments,
+                                        :likes).order(created_at: :desc)
+
+    visible_posts.each do |post|
+      expect(page).to have_content(post.title)
+      expect(page).to have_content(post.text.truncate(100)) # Assuming you show a truncated body
+      expect(page).to have_content("Comments: #{post.comments.count}")
+      expect(page).to have_content("Likes: #{post.likes.count}")
+      post.recent_comments.each do |comment|
+        expect(page).to have_content(comment.user.name)
+        expect(page).to have_content(comment.text)
+        unique_comment_content = "#{comment.user.name}: #{comment.text}"
+        expect(page).to have_content(unique_comment_content)
+      end
+    end
+  end
+
+  it 'shows pagination if there are more posts than fit on the view' do
+    expect(page).to have_selector('.pagination')
+    expect(page).to have_link('2') # Checking for a link to the second page
+  end
+
+  it 'redirects to a post show page when a post is clicked' do
+    first_post = user.posts.includes(:comments, :likes).order(created_at: :desc).first
+    find('a', text: first_post.title, match: :prefer_exact).click
+    expect(current_path).to eq(user_post_path(user, first_post))
+  end
 end
--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -1,13 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'User Show Page', type: :feature do
-  # let(:user) do
-  # end
   before do
     @user = User.create(name: 'Abdu', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Yemen.',
-                        posts_counter: 0)
-    # post = Post.create(author: @user, title: 'Harru', text: 'text', comments_counter: 0,
-    #                    likes_counter: 0)
+                        posts_counter: 1)
+
     visit user_path(@user)
   end
 
@@ -24,16 +21,16 @@ RSpec.describe 'User Show Page', type: :feature do
     end
   end
 
-  it 'redirects to the posts index page when view all posts button is clicked' do
+  it 'redirects to the posts index page when posts button is clicked' do
     click_on 'posts'
     expect(current_path).to eq(user_posts_path(@user))
   end
 
   it 'redirects to a post show page when a post is clicked' do
-    post = @user.posts.create(title: 'Sample Title', text: 'Sample Text')
-
-    link_text = 'Post'
-    expect(page).to have_link(link_text)
+    post = @user.posts.create(author: @user, title: 'Hello', text: 'text', comments_counter: 0,
+                              likes_counter: 0)
+    link_text = post.title
+    # expect(page).to have_link(link_text)
 
     click_link(link_text)
     expect(current_path).to eq(user_post_path(@user, post))
