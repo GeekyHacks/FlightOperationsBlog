3e9efcf1908f04c84822de107736c7ea68b6a661
Abdullah <geekyhacks22@gmail.com>
2024-02-02 18:24:48 +0300

    Add user_posts_index test case

--- spec/features/user_posts_index_spec.rb
+++ spec/features/user_posts_index_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'User Post Index Page', type: :feature do
+  let(:user) { User.first }
   before do
-    @user = User.create(name: 'Abdu', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Yemen.',
-                        posts_counter: 0)
-    11.times { |i| Post.create(author: @user, title: "Post #{i}", text: "Content of post #{i}") }
+    11.times { |i| Post.create(author: user, title: "Post #{i}", text: "Content of post #{i}") }
     visit user_posts_path(user)
   end
 
+
   it 'shows the user and posts details' do
-    expect(page).to have_selector("img[src*='#{@user.photo}']")
-    expect(page).to have_content(@user.name)
-    expect(page).to have_content("Number of posts: #{@user.posts_counter}")
+    expect(page).to have_selector("img[src*='#{user.photo}']")
+    expect(page).to have_content(user.name)
+    expect(page).to have_content("Number of Posts:#{user.posts_counter}")
 
     visible_posts = user.posts.includes(:comments,
                                         :likes).order(created_at: :desc)
@@ -28,11 +28,6 @@ RSpec.describe 'User Post Index Page', type: :feature do
         expect(page).to have_content(unique_comment_content)
       end
     end
-  end
-
-  it 'shows pagination if there are more posts than fit on the view' do
-    expect(page).to have_selector('.pagination')
-    expect(page).to have_link('2') # Checking for a link to the second page
   end
 
   it 'redirects to a post show page when a post is clicked' do
